37d1d2a505bc217f60cd11adf27f412d2cea077e
Caleb Sayre <DrSayre2002@yahoo.com>
2017-01-02 00:37:48 -0500

    using enviromental variables

--- config/initializers/setup_twitter.rb
+++ config/initializers/setup_twitter.rb
@@ -1,6 +1,6 @@
 $twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key        = 'uKgW4n7E3oAE2HbiTPwRvIclQ'
-  config.consumer_secret     = 'hDNjozw27Vc274UlUtCaOLRpj7L79LAYJmOmcwrzyNkqQY5wJc'
-  config.access_token        = '815713887899230208-vcITiu3iokQEGEeiOlVIenSyjzHZG8w'
-  config.access_token_secret = 'aqUX8PdNZq6uQaeg2Zr8Kv30rZz0evEN2w3EmDFnbEsLz'
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
