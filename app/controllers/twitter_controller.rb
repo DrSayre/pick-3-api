8c12b146f03b8076524617e11fb4b9993a3e1c01
Caleb Sayre <DrSayre2002@yahoo.com>
2017-01-02 17:46:23 -0500

    add ability to update databases

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -10,4 +10,39 @@ class TwitterController < ApplicationController
     $twitter.update("Tonight's drawing: #{numbers.join(', ')} #KYPick3")
     render status: 204
   end
+
+  def update
+    midday = DrawingTime.where(name: 'Midday').first_or_create
+    evening = DrawingTime.where(name: 'Evening').first_or_create
+    save_numbers(midday, 'Midday-Pick-3')
+    save_numbers(evening, 'Pick-3')
+  end
+
+  def save_numbers(drawing_time, drawing)
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    last_year = PickThree.maximum(:drawing_date).year
+    (last_year..Time.now.year).each do |year|
+      months.each do |month|
+        url = "https://www.alllotto.com/Kentucky-#{drawing}-#{month}-#{year}-Lottery-Results.php"
+        page = HTTParty.get(url)
+        parse_page = Nokogiri::HTML(page)
+        table = parse_page.css('table')[4]
+        table_rows = table.css('tr')
+        table_rows.each do |row|
+          if row.css('td').first.text.include? 'Pick 3'
+            date_parts = row.css('td')[1].text.split('-')
+            date = Date.new(date_parts[2].to_i, date_parts[0].to_i, date_parts[1].to_i)
+            numbers = row.css('td')[2].text.split(',')
+            numbers.map!{ |number| number.delete(' ')}
+            begin
+              drawing_time.pick_threes.create(first_ball: numbers[0], second_ball: numbers[1], third_ball: numbers[2], drawing_date: date)
+            rescue
+              print "\n------\n#{date} - #{numbers.inspect} Already recorded\n------\n"
+            end
+            print "\n------\n#{date} - #{numbers.inspect}\n------\n" 
+          end
+        end
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'twitter/evening', to: 'twitter#evening'
-  post 'twitter/midday', to: 'twitter#midday' 
+  post 'twitter/midday', to: 'twitter#midday'
+  post 'twitter/update', to: 'twitter#update'
 end
